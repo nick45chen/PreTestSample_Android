1b1b7c3801a10821fe1f63a97882c34d8b5b88d0
nick_chen <e7810510@gamil.com>
2020-01-14 16:54:32 +0800

    1.[新增] Webview Activity

---
+++ app/src/main/java/com/example/fuglepretestsample/views/web/WebViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fuglepretestsample.views.web
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fuglepretestsample.R
+
+class WebViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
